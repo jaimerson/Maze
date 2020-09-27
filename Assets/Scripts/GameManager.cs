ff416d5d9506694d41a9293bcd90afecaccc3174
James <jaimersonaraujo@gmail.com>
2020-09-27 13:22:14 -0300

    Add timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Maze mazePrefab;
     private Maze maze;
+    public Text text;
+    public Text level;
+    public int initialSeconds;
+    private int secondsLeft;
+    private int currentLevel;
     // Start is called before the first frame update
     void Start()
     {
+        currentLevel = 1;
+        secondsLeft = initialSeconds;
+        level.text = "Level: " + currentLevel;
+        StartCoroutine(timer());
         BeginGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        text.text = secondsLeft.ToString();
+
+        if (secondsLeft <= 0)
         {
             RestartGame();
+        }
+    }
+
+    private IEnumerator timer()
+    {
+        while (true)
+        {
+            secondsLeft -= 1;
+            yield return new WaitForSeconds(1);
         }
     }
 
     public void NextLevel()
     {
         EndGame();
+        secondsLeft += this.maze.size.x * 2;
+        currentLevel += 1;
+        level.text = "Level: " + currentLevel;
 
         IntVector2 nextSize = this.maze.size + new IntVector2(1, 1);
         this.maze = Instantiate(mazePrefab) as Maze;
