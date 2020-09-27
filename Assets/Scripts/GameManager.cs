6bccb898b3afd7f2bf47a256d23f0d2dc2710720
James <jaimersonaraujo@gmail.com>
2020-09-27 11:16:39 -0300

    Maze generation working

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Maze mazePrefab;
+    private Maze maze;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeginGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame()
+    {
+        this.maze = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(maze.Generate());
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(this.maze.gameObject);
+        BeginGame();
+    }
+}
